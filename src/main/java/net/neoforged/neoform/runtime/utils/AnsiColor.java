4a4e435bc2aa76709081c0489007c20a69861501
Bruno Ploumhans <13494793+Technici4n@users.noreply.github.com>
2026-08-31 23:50:14 +0200

    Fix #129: AnsiColor.RESET now properly clears italic in IDEA (#130)

--- src/main/java/net/neoforged/neoform/runtime/utils/AnsiColor.java
+++ src/main/java/net/neoforged/neoform/runtime/utils/AnsiColor.java
@@ -2,8 +2,8 @@ package net.neoforged.neoform.runtime.utils;
 
 // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
 public enum AnsiColor {
-    //Color end string, color reset
-    RESET("0"),
+    // 23 to disable italic (0 doesn't do it in IDEA); 0 to reset everything (in theory)
+    RESET("23;0"),
 
     YELLOW("33"),
 
